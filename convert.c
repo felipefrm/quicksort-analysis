269073d97a6c57e5d9a2e5097c1910a33e8430d5
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2018-11-02 20:46:29 -0300

    Update convert.c

--- convert.c
+++ convert.c
@@ -4,17 +4,21 @@
 #include <math.h>
 #include "convert.h"
 
+#define a 48
+#define z 57
+#define base 10
+
 int transformaChar(char num){
 
-  if (num >= 48 && num <= 57)
-    num = num - 48;
+  if (num >= a && num <= z)
+    num = num - a;
   return num;
 }
 
 int CharParaInt(char *charN){
   int soma = 0, expoente = strlen(charN)-1;
   for (int i = 0; i < strlen(charN); i++){
-    soma += transformaChar(charN[i]) * pow(10, expoente);
+    soma += transformaChar(charN[i]) * pow(base, expoente);
     expoente--;
   }
   return soma;
