5c0ef5bc9949e12743891156ac5e9db576f5ed71
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2018-11-02 16:41:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/resource.h>
 // #include "lista.h"
 #include "quicksort.h"
 #include "convert.h"
 #include "funcoes.h"
 
-#define vet 0
-#define strct 1
-#define list
-#define repeat 5
+#define VET 0
+#define STRCT 1
+#define LIST 2
+#define ENTRADA 3
+#define REPEAT 5
+#define INCREASE 10
 
 int main(int argc, char** argv){
-  struct rusage resources;
   double utime_ant, utime_pos, stime_ant, stime_pos;
   if (argc > 3) {
 
@@ -26,9 +26,9 @@ int main(int argc, char** argv){
     leituraParametros(input, &qtdN, N);
     // le quantidade de parametros e o valor de cada um
 
-    // printf("%d\n", qtdN);
-    // for(int i=0; i<qtdN; i++)
-    //   printf("%d\n", N[i]);
+    printf("%d\n", qtdN);
+    for(int i=0; i<qtdN; i++)
+      printf("%c\n", N[i]);
     // char c, vetN[2];  //lendo a quantidade de N
     // for(int i=0; (c=fgetc(input)) != '\n'; i++)  // o valor é lido como char
     //   vetN[i] = c;
@@ -47,84 +47,73 @@ int main(int argc, char** argv){
 
     fclose(input);
 
-    for (int x=0; x<qtdN; x++){
+    for (int i=0; i<qtdN; i++){
 
-      fprintf(output, "-------------------------------\nPara N = %d\n\n", N[x]);
-      int *vetor = (int*) malloc(N[x]*sizeof(int));
-      elemento* vStruct = (elemento*)malloc(N[x]*sizeof(elemento));
+      fprintf(output, "-------------------------------\nPara N = %d\n\n", N[i]);
+      int *vetor = (int*) malloc(N[i]*sizeof(int));
+      elemento* vStruct = (elemento*)malloc(N[i]*sizeof(elemento));
       // Lista* li = criaLista();
 
-      double cmpmed[3]={0,0,0}, trcmed[3]={0,0,0};
-      double total_time[3] = {0,0,0};
+      double cmpmed[ENTRADA]={0,0,0}, trcmed[ENTRADA]={0,0,0};
+      double total_time[ENTRADA] = {0,0,0};
 
       int seed = atoi(argv[1]);   // declara a seed recebendo o argumento 1
 
-      for (int y=0; y<repeat; y++){
-        randomVetor(vetor, N[x]);
-        randomStruct(vStruct, N[x]);
+      for (int j=0; j<REPEAT; j++){
+        randomVetor(vetor, N[i]);
+        randomStruct(vStruct, N[i]);
         // randomLista(li, vetor, N)
-        srand(seed+y*10); //seta a seed e incrementa 10 a cada repetição
+        srand(seed+j*INCREASE); //seta a seed e incrementa 10 a cada repetição
 
         // printf("Vetor inicial:\n");
-        // for (int i=0; i < N[x]; i++)
+        // for (int i=0; i < N[i]; i++)
         //   printf("%d\n", vetor[i]);
         // printf("\n");
 
-        int numAtivacoes = 0, compara = 0, troca = 0;
-        getrusage(RUSAGE_SELF, &resources);
-        utime_ant = (double) resources.ru_utime.tv_sec + 1.e-6 * (double) resources.ru_utime.tv_usec;
-        stime_ant = (double) resources.ru_stime.tv_sec + 1.e-6 * (double) resources.ru_stime.tv_usec;
-        quickInt(vetor,0,N[x]-1,&numAtivacoes, &compara, &troca);
+        int compara = 0, troca = 0;
+        contaTempo(&utime_ant, &stime_ant); //marca tempo inicial
+        quickInt(vetor, 0, N[i]-1, &compara, &troca);
+        contaTempo(&utime_pos, &stime_pos); //marca tempo final
 
-        getrusage(RUSAGE_SELF, &resources);
-        utime_pos = (double) resources.ru_utime.tv_sec + 1.e-6 * (double) resources.ru_utime.tv_usec;
-        stime_pos = (double) resources.ru_stime.tv_sec + 1.e-6 * (double) resources.ru_stime.tv_usec;
-
-        total_time[vet] += ((utime_pos-utime_ant) + (stime_pos-stime_ant));
+        total_time[VET] += ((utime_pos-utime_ant) + (stime_pos-stime_ant)); //somatorio do tempo total para vetor
+        trcmed[VET] += troca;   //somatorio das trocas para vetor
+        cmpmed[VET] += compara; //somatorio das comparações para vetor
 
         // printf("Vetor ordenado:\n");
-        // for (int i=0; i < N[x]; i++)
+        // for (int i=0; i < N[i]; i++)
         //   printf("%d\n", vetor[i]);
         // printf("\n");
 
-        cmpmed[vet] += compara;
-        trcmed[vet] += troca;
 
         // printf("Vetor de struct inicial:\n");
-        // for (int i=0; i < N[x]; i++){
-        //   // printf("%d %d\n", vStruct[i].ch, vStruct[i].boleano);
-        //   for (int j=0; j < 10; j++){
-        //
-        //    printf("str%d: %s\n\n", j, vStruct[i].str[j]);
-        //    printf("\n%ld\n", strlen(vStruct[i].str[j]));
+        // for (int m=0; m < N[i]; m++){
+        //   printf("%d %d\n", vStruct[m].ch, vStruct[m].boleano);
+        //   // for (int t=0; t < 10; t++){
+        //   //
+        //    // printf("str%d: %s\n\n", t, vStruct[r].str[t]);
+        // // }
+        //   for (int n=0; n < 4; n++)
+        //    printf("%f ", vStruct[n].f[m]);
+        //   printf("\n");
         //  }
-          // for (int j=0; j < 4; j++)
-          //  printf("%f ", vStruct[i].f[j]);
-          // printf("\n");
-        // }
         // printf("\n");
 
-        numAtivacoes = compara = troca = 0;
-        getrusage(RUSAGE_SELF, &resources);
-        utime_ant = (double) resources.ru_utime.tv_sec + 1.e-6 * (double) resources.ru_utime.tv_usec;
-        stime_ant = (double) resources.ru_stime.tv_sec + 1.e-6 * (double) resources.ru_stime.tv_usec;
-        quickStruct(vStruct,0,N[x]-1,&numAtivacoes, &compara, &troca);
+        compara = troca = 0;
+        contaTempo(&utime_ant, &stime_ant);  //marca o tempo inicial
+        quickStruct(vStruct, 0, N[i]-1, &compara, &troca);
+        contaTempo(&utime_pos, &stime_pos); //marca o tempo final
 
-        getrusage(RUSAGE_SELF, &resources);
-        utime_pos = (double) resources.ru_utime.tv_sec + 1.e-6 * (double) resources.ru_utime.tv_usec;
-        stime_pos = (double) resources.ru_stime.tv_sec + 1.e-6 * (double) resources.ru_stime.tv_usec;
-
-        total_time[strct] += ((utime_pos-utime_ant) + (stime_pos-stime_ant));
+        total_time[STRCT] += ((utime_pos-utime_ant) + (stime_pos-stime_ant)); //somatorio do tempo total para struct
+        trcmed[STRCT] += troca;   //somatorio das trocas para struct
+        cmpmed[STRCT] += compara; //somatorio das comparações para struct
 
         // printf("Vetor de struct ordenado:\n");
-        for (int i=0; i < N[x]; i++){
+        // for (int i=0; i < N[i]; i++){
           // printf("%d %d\n", vStruct[i].ch, vStruct[i].boleano);
           //for (int j=0; j < K; j++)
           //  printf("str%d: %s\n", j, vStruct[i].str[j]);
           //printf("\n");
-        }
-        trcmed[strct] += troca;
-        cmpmed[strct] += compara;
+        // }
         // printf("Número de ativações: %d\nNúmero de comparação de chaves: %d\nNúmero de troca de registros: %d\n\n", numAtivacoes, compara, troca);
 
 
@@ -145,12 +134,12 @@ int main(int argc, char** argv){
       free(vetor);
       free(vStruct);
 
-      // fprintf(output, "Para N = %d\n\nVetor:\nComparações de chaves: %d\nTrocas de registros: %d", N[x], )
+      // fprintf(output, "Para N = %d\n\nVetor:\nComparações de chaves: %d\nTrocas de registros: %d", N[i], )
       //
-      // fprintf(output, "Para N = %d, a media do tempo de execução em vetor foi de: %fs,\n"
-      // "para struct foi de: %fs.\n", N[x], vet_soma_time/(float)repeat, strct_soma_time/(float)repeat);
-      fprintf(output, "Vetor:\nComparações de chaves: %f\nTrocas de registros: %f\nTempo total gasto na ordenação: %fs\n\n", cmpmed[vet]/5.0, trcmed[vet]/5.0, total_time[vet]/5.0);
-      fprintf(output, "Vetor de struct:\nComparações de chaves: %f\nTrocas de registros: %f\nTempo total gasto na ordenação: %fs\n\n", cmpmed[strct]/5.0, trcmed[strct]/5.0, total_time[strct]/5.0);
+      // fprintf(output, "Para N = %d, a media do tempo de eiecução em vetor foi de: %fs,\n"
+      // "para struct foi de: %fs.\n", N[i], vet_soma_time/(float)repeat, strct_soma_time/(float)repeat);
+      fprintf(output, "Vetor:\nComparações de chaves: %f\nTrocas de registros: %f\nTempo total gasto na ordenação: %fs\n\n", cmpmed[VET]/5.0, trcmed[VET]/5.0, total_time[VET]/5.0);
+      fprintf(output, "Vetor de struct:\nComparações de chaves: %f\nTrocas de registros: %f\nTempo total gasto na ordenação: %fs\n\n", cmpmed[STRCT]/5.0, trcmed[STRCT]/5.0, total_time[STRCT]/5.0);
       }
 
   fclose(output);
@@ -161,8 +150,6 @@ int main(int argc, char** argv){
 }
 
 /* FALTA:
-ajeitar o time
 quick pra lista
-modularizar
 constantes
 */
