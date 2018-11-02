a7d12424e1791f50806bc1f2e7ebd46bc893a5da
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2018-11-02 20:44:43 -0300

    Update and rename funcoes.h to analisys.h

--- analisys.h
+++ analisys.h
@@ -1,10 +1,11 @@
-#ifndef FUNCOES
-#define FUNCOES
+#ifndef ANALISYS
+#define ANALISYS
 
 void randomVetor(int *v, int N);
 void randomStruct(elemento *v, int N);
 // void randomLista(Lista *li, int *v, int N)
 int* leituraParametros(FILE *input, int *qtdN, int *N);
 void contaTempo(double *utime, double *stime);
+void computaEstatisticas(double *tmpmed, double *trcmed, double *cmpmed, int tipo, double tempo, int troca, int compara);
 
 #endif
