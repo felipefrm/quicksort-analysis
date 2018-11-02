6cc65423719bfaca15fc1dfdf4d3e8dbbc88b575
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2018-11-02 16:44:08 -0300

    Update quicksort.h

--- quicksort.h
+++ quicksort.h
@@ -2,7 +2,7 @@
 #define QUICKSORT
 
 #define qntstr 10
-#define sizestr 200
+#define sizestr 201   // 200+1 considerando o terminador de string
 #define qntfloat 4
 
 typedef struct TElemento {
@@ -13,9 +13,9 @@ typedef struct TElemento {
 } elemento;
 
 int particionaStruct(elemento* vetor, int inicio, int fim, int *compara, int *troca);
-void quickStruct(elemento *vetor, int inicio, int fim, int *cont, int *compara, int *troca);
+void quickStruct(elemento *vetor, int inicio, int fim, int *compara, int *troca);
 int particionaInt(int *vetor, int inicio, int fim, int *compara, int *troca);
-void quickInt(int *vetor, int inicio, int fim, int *cont, int *compara, int *troca);
+void quickInt(int *vetor, int inicio, int fim, int *compara, int *troca);
 // int particionaLista(Lista* li, int inicio, int fim);
 // void quickLista(Lista* li, int inicio, int fim, int *cont);
 
