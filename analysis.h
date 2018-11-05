f7d61732dfcf56988a5a855b85701a3b10cca886
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2018-11-05 12:41:17 -0200

    Update analysis.h

--- analysis.h
+++ analysis.h
@@ -1,11 +1,17 @@
 #ifndef ANALYSIS
 #define ANALYSIS
 
+#define VET 0
+#define STRCT 1
+#define LIST 2
+
 void randomVetor(int *v, int N);
 void randomStruct(elemento *v, int N);
 // void randomLista(Lista *li, int *v, int N)
 int* leituraParametros(FILE *input, int *qtdN, int *N);
 void contaTempo(double *utime, double *stime);
 void computaEstatisticas(double *tmpmed, double *trcmed, double *cmpmed, int tipo, double tempo, int troca, int compara);
+void imprimeMediaArq(FILE *output, double *cmpmed, double *trcmed, double *tmpmed, int repeat);
+
 
 #endif
