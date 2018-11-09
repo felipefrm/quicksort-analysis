a62f0ba63087cdc9451c4ae96855f622373f0358
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2018-11-09 21:12:55 -0200

    Update analysis.h

--- analysis.h
+++ analysis.h
@@ -7,7 +7,7 @@
 
 void randomVetor(int *v, int N);
 void randomStruct(elemento *v, int N);
-// void randomLista(Lista *li, int *v, int N)
+void randomLista(Lista *li, int n);
 int* leituraParametros(FILE *input, int *qtdN);
 void contaTempo(double *utime, double *stime);
 void computaEstatisticas(double *tmpmed, double *trcmed, double *cmpmed, int tipo, double tempo, int troca, int compara);
