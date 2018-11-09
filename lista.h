b106d9d3be6654a556369709ee7b17d485747f0c
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2018-11-09 21:12:17 -0200

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,11 +1,21 @@
 #ifndef LISTA
 #define LISTA
 
+//Definição do tipo lista
+struct elemento{
+    struct elemento *ant;
+    struct elemento *prox;
+    int num;
+};
+typedef struct elemento Elem;
 typedef struct elemento* Lista;
+
 
 Lista* criaLista();
 void liberaLista(Lista* li);
 int insereLista(Lista* li, int num);
 void imprimeLista(Lista* li);
+void swap(int* a,int* b);
+Elem* ultimono(Lista* li);
 
 #endif
