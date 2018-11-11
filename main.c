f9819877f459a49ff00572c645293e7dad446040
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2018-11-11 13:08:57 -0200

    Update main.c

--- main.c
+++ main.c
@@ -20,11 +20,12 @@ int main(int argc, char** argv){
     FILE *output;
     output = fopen(argv[3], "w");
 
+    fprintf(output, "\n>>>   MÉDIA DAS MÉTRICAS   <<<\n\n");
     for (int i=0; i<qtdN; i++){
-      fprintf(output, "-------------------------------\nPara N = %d\n\n", N[i]);
+      fprintf(output, "-------------------------------\n\nPara N = %d\n\n", N[i]);
       int *vetor = (int*) malloc(N[i]*sizeof(int));
-      elemento *vStruct = (elemento*)malloc(N[i]*sizeof(elemento));
       Lista* li = criaLista();
+      elemento *vStruct = (elemento*)malloc(N[i]*sizeof(elemento));
       // Alocação na memoria para os 3 tipos
 
       double cmpmed[TYPE]={0,0,0}, trcmed[TYPE]={0,0,0}, tmpmed[TYPE] = {0,0,0};
@@ -32,10 +33,9 @@ int main(int argc, char** argv){
 
       for (int j=0; j<REPEAT; j++){
         srand(seed+j*INCREASE); //seta a seed e incrementa 10 a cada repetição
-        fprintf(output, ">>> %dª repetição\n", j+1);
         randomVetor(vetor, N[i]);
-        randomStruct(vStruct, N[i]);    // gerador de valores aleatorios
         randomLista(li, vetor, N[i]);
+        randomStruct(vStruct, N[i]);    // gerador de valores aleatorios
 
         // vetor de inteiros
         double utime_ant, utime_pos, stime_ant, stime_pos, diftempo;
@@ -45,16 +45,6 @@ int main(int argc, char** argv){
         contaTempo(&utime_pos, &stime_pos); //marca tempo final
         diftempo = (utime_pos-utime_ant) + (stime_pos-stime_ant);
         computaEstatisticas(tmpmed, trcmed, cmpmed, VET, diftempo, troca, compara);
-        fprintf(output, "- Vetor de inteiros\n   Comparações de chaves: %d\n   Trocas de registros: %d\n   Tempo gasto: %fs\n\n", compara, troca, diftempo);
-
-        // vetor de struct
-        compara = troca = 0;
-        contaTempo(&utime_ant, &stime_ant);  //marca o tempo inicial
-        quickStruct(vStruct, 0, N[i]-1, &compara, &troca);
-        contaTempo(&utime_pos, &stime_pos); //marca o tempo final
-        diftempo = (utime_pos-utime_ant) + (stime_pos-stime_ant);
-        computaEstatisticas(tmpmed, trcmed, cmpmed, STRCT, diftempo, troca, compara);
-        fprintf(output, "- Vetor de Struct:\n   Comparações de chaves: %d\n   Trocas de registros: %d\n   Tempo gasto: %fs\n\n", compara, troca, diftempo);
 
         // lista duplamente encadeada de inteiros
         compara = troca = 0;
@@ -63,13 +53,20 @@ int main(int argc, char** argv){
         contaTempo(&utime_pos, &stime_pos); //marca o tempo final
         diftempo = (utime_pos-utime_ant) + (stime_pos-stime_ant);
         computaEstatisticas(tmpmed, trcmed, cmpmed, LIST, diftempo, troca, compara);
-        fprintf(output, "- Lista duplamente encadeada de inteiros\n   Comparações de chaves: %d\n   Trocas de registros: %d\n   Tempo gasto: %fs\n\n", compara, troca, diftempo);
         esvaziaLista(li);  //esvazia lista para inserir novos elementos
+
+        // vetor de struct
+        compara = troca = 0;
+        contaTempo(&utime_ant, &stime_ant);  //marca o tempo inicial
+        quickStruct(vStruct, 0, N[i]-1, &compara, &troca);
+        contaTempo(&utime_pos, &stime_pos); //marca o tempo final
+        diftempo = (utime_pos-utime_ant) + (stime_pos-stime_ant);
+        computaEstatisticas(tmpmed, trcmed, cmpmed, STRCT, diftempo, troca, compara);
       }
 
       free(vetor);
-      free(vStruct);    // libera os ponteiros
       liberaLista(li);
+      free(vStruct);    // libera os ponteiros
 
       imprimeMediaArq(output, cmpmed, trcmed, tmpmed, REPEAT);  // imprime media
     }
@@ -77,8 +74,9 @@ int main(int argc, char** argv){
     fclose(output);
     free(N);
   }
-  
+
   else
     printf("Digite todos argumentos na linha de argumento\n");
+
   return 0;
 }
